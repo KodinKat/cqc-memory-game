36f0bbc8316506e82ccf34de17452e0c7bcef08f
KodinKat <me@karlkatewu.com>
2016-07-16 21:36:27 +0100

    Travis-CI

--- src/com/shmoop/cqc/game/MemoryGameApp.java
+++ src/com/shmoop/cqc/game/MemoryGameApp.java
@@ -1,13 +1,14 @@
 package com.shmoop.cqc.game;
 
-import com.shmoop.cqc.game.ui.MemoryGameUI;
+//import com.shmoop.cqc.game.ui.MemoryGameUI;
 
 public class MemoryGameApp {
 
 	public static void main(String[] args) {
 		
-		MemoryGameUI memoryGameUI = new MemoryGameUI();
-		MemoryGameUI.launch(memoryGameUI.getClass(), args);
+		//MemoryGameUI memoryGameUI = new MemoryGameUI();
+		//MemoryGameUI.launch(memoryGameUI.getClass(), args);
+		System.out.println("Hello Continuous Integration!!!");
 
 	}
 

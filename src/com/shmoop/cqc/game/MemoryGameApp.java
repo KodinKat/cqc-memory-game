d641c0a4232db4a6066176ff5e9678a20ec0d423
KodinKat <me@karlkatewu.com>
2016-07-16 22:31:41 +0100

    L14 Continuous Integration (CI)

--- src/com/shmoop/cqc/game/MemoryGameApp.java
+++ src/com/shmoop/cqc/game/MemoryGameApp.java
@@ -1,15 +1,14 @@
 package com.shmoop.cqc.game;
 
-//import com.shmoop.cqc.game.ui.MemoryGameUI;
+import com.shmoop.cqc.game.ui.MemoryGameUI;
 
 public class MemoryGameApp {
 
 	public static void main(String[] args) {
 		
-		//MemoryGameUI memoryGameUI = new MemoryGameUI();
-		//MemoryGameUI.launch(memoryGameUI.getClass(), args);
-		System.out.println("Hello Continuous Integration!!!");
-
+		MemoryGameUI memoryGameUI = new MemoryGameUI();
+		MemoryGameUI.launch(memoryGameUI.getClass(), args);
+		
 	}
 
 }
